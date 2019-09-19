524e4040168fd043ccc8767cb9923e1ca9e29fc2
Roman_v <vigmol@ukr.net>
2019-09-19 22:41:38 +0300

    add interface repository

---
+++ AsItShouldBeFromScratch/src/main/java/net/com/AsItShouldBeFromScratch/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package net.com.AsItShouldBeFromScratch.repository;
+
+import net.com.AsItShouldBeFromScratch.modal.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
