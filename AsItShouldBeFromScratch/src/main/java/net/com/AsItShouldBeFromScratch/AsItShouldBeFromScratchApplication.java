e5052e436470c730b6a5ee4d628a544cb33f3b3a
Roman_v <vigmol@ukr.net>
2019-09-19 22:24:38 +0300

    Initial commit

---
+++ AsItShouldBeFromScratch/src/main/java/net/com/AsItShouldBeFromScratch/AsItShouldBeFromScratchApplication.java
@@ -0,0 +1,13 @@
+package net.com.AsItShouldBeFromScratch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsItShouldBeFromScratchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsItShouldBeFromScratchApplication.class, args);
+	}
+
+}
